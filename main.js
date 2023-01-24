bc37041f794fa670ad512bf816f3c54191234a7d
Choaib Elmadi <111356633+Choaib-ELMADI@users.noreply.github.com>
2023-01-24 18:46:24 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,13 @@
+const myAtropos = Atropos({
+
+    // ! Add custom props 
+
+    el: '.my-atropos',
+    rotateXMax   : 30,
+    rotateYMax   : 20,
+    shadow       : true,
+    shadowOffSet : 2,
+    shadowScale  : 0.9,
+    durationEnter: 500,
+    activeOffSet : 100
+});
